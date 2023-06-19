0100a2ef81fbd7ed0f118d17f977dc41cc34ebed
mvuqar1 <vuqar.5100@mail.ru>
2023-06-19 18:34:04 +0400

    md

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { SearchOutlined, HomeOutlined, ShoppingCartOutlined, FileTextOutlined, UserOutlined, BarChartOutlined, LogoutOutlined } from '@ant-design/icons';
-import { Input } from 'antd';
+import { Badge, Input } from 'antd';
 
 export default function Header() {
     return (
@@ -11,35 +11,44 @@ export default function Header() {
                         <h2 className='text-2xl font-bold md:text-4xl'>LOGO</h2>
                     </a>
                 </div>
-                <div className="header-search flex-1">
+                <div className="header-search flex-1 flex justify-center">
                     <Input size="large" placeholder="Search items" prefix={<SearchOutlined />} className='rounded-full max-w-[800px]' />
                 </div>
-                <div className="menu-links flex justify-between items-center gap-8">
-                    <a href={"menu-link"} className='flex flex-col'>
+
+                <div className="menu-links flex justify-between items-center gap-7 md:static fixed bottom-0 md:w-auto w-screen md:bg-transparent bg-white left-0 md:border-t-0 border-t md:px-0 px-4 py-1">
+                    <a href={"menu-link"} className='flex flex-col hover:text-[#40a9ff] transition-all'>
                         <HomeOutlined className='md:text-2xl text-2xl' />
                         <span className='md:text-xs text-[10px]'>Home</span>
                     </a>
-                    <a href={"menu-link"} className='flex flex-col'>
-                        <ShoppingCartOutlined className='md:text-2xl text-2xl' />
-                        <span className='md:text-xs text-[10px]'>Korzina</span>
-                    </a>
-                    <a href={"menu-link"} className='flex flex-col'>
+                    <Badge count={5} offset={[0, 6]} className='md:flex hidden'>
+                        <a href={"menu-link"} className='flex flex-col hover:text-[#40a9ff] transition-all'>
+                            <ShoppingCartOutlined className='md:text-2xl' />
+                            <span className='md:text-xs text-[10px]'>Korzina</span>
+                        </a>
+                    </Badge>
+                    <a href={"menu-link"} className='flex flex-col hover:text-[#40a9ff] transition-all'>
                         <FileTextOutlined className='md:text-2xl text-2xl' />
                         <span className='md:text-xs text-[10px]'>Fatura</span>
                     </a>
-                    <a href={"menu-link"} className='flex flex-col'>
+                    <a href={"menu-link"} className='flex flex-col hover:text-[#40a9ff] transition-all'>
                         <UserOutlined className='md:text-2xl text-2xl' />
                         <span className='md:text-xs text-[10px]'>Muwteri</span>
                     </a>
-                    <a href={"menu-link"} className='flex flex-col'>
+                    <a href={"menu-link"} className='flex flex-col hover:text-[#40a9ff] transition-all'>
                         <BarChartOutlined className='md:text-2xl text-2xl' />
                         <span className='md:text-xs text-[10px]'>Statiks</span>
                     </a>
-                    <a href={"menu-link"} className='flex flex-col'>
+                    <a href={"menu-link"} className='flex flex-col hover:text-[#40a9ff] transition-all'>
                         <LogoutOutlined className='md:text-2xl text-2xl' />
                         <span className='md:text-xs text-[10px]'>Exit</span>
                     </a>
                 </div>
+                <Badge count={5} offset={[0, 6]} className='md:hidden flex'>
+                    <a href={"menu-link"} className='flex flex-col hover:text-[#40a9ff] transition-all'>
+                        <ShoppingCartOutlined className='text-2xl' />
+                        <span className='md:text-xs text-[10px]'>Korzina</span>
+                    </a>
+                </Badge>
             </header>
         </div>
     )
